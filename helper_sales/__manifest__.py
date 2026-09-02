7bbd0ebc73f4b446f3a8ee979f622d10ccbf5c41
MsamyAzizy <azizomary99@gmail.com>
2026-09-02 23:42:59 +0300

    fix:Batch invoice genearate id

--- helper_sales/__manifest__.py
+++ helper_sales/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Helper Sales",
-    "version": "18.0.1.0.0",
+    'version': '18.0.1.2.0',
     "category": "Sales",
     "summary": "Add Helper field to Sales Orders",
     "description": """
