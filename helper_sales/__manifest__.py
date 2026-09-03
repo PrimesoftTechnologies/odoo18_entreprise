bb9d8607b6ec24648eba6df4fac2eba757b318ee
MsamyAzizy <azizomary99@gmail.com>
2026-09-03 11:08:35 +0300

    fix:helper new fields, car number, driver etc

--- helper_sales/__manifest__.py
+++ helper_sales/__manifest__.py
@@ -2,8 +2,9 @@
 
 {
     "name": "Helper Sales",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.1",
     "category": "Sales",
+    "sequence": -28,
     "summary": "Add Helper field to Sales Orders",
     "description": """
         Adds a Helper field to Sales Orders.
@@ -15,9 +16,11 @@
         "hr",
     ],
     "data": [
+        "security/ir.model.access.csv", 
         "views/sale_order_views.xml",
+        "reports/invoice_report.xml",
     ],
     "installable": True,
-    "application": False,
+    "application": True,
     "license": "LGPL-3",
 }
--- helper_sales/models/sale_order.py
+++ helper_sales/models/sale_order.py
@@ -1,10 +1,37 @@
-from odoo import models, fields
+from odoo import fields, models
+
+class SaleRegion(models.Model):
+    _name = 'sale.region'
+    _description = 'Sales Region'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Region Name', required=True, tracking=True)
+    code = fields.Char(string='Region Code', tracking=True)
+
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
+    driver_id = fields.Many2one(
+        "hr.employee",
+        string="Driver Name",
+        required=True,
+        tracking=True,
+    )
     helper_id = fields.Many2one(
-        'hr.employee', 
-        string='Helper', 
-        tracking=True
+        "hr.employee",
+        string="Helper",
+        required=True,
+        tracking=True,
+    )
+    car_number = fields.Char(
+        string="Car Number",
+        required=True,
+        tracking=True,
+    )
+    region_id = fields.Many2one(
+        "sale.region",
+        string="Region",
+        required=True,
+        tracking=True,
     )
