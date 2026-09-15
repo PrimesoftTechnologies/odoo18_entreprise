d22370f407dad6ca1e65faa7938d42425985b6bf
MsamyAzizy <azizomary99@gmail.com>
2026-09-15 13:32:23 +0300

    feat:apex module

--- apex_sales_custom/models/sale_order.py
+++ apex_sales_custom/models/sale_order.py
@@ -20,7 +20,7 @@ class SaleOrder(models.Model):
             if order.state in ['draft', 'sent'] and order.id:
                 order.revision_number += 1
                 rev_code = f"REV-{order.revision_number:02d}"
-                order.message_post(body=f"Order revised to {rev_code}. Reason/Change: {change_description}")
+                order.message_post(body=f"Order Code revised to {rev_code}. Reason/Changes: {change_description}")
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
@@ -31,7 +31,7 @@ class SaleOrderLine(models.Model):
         for line in lines:
             if line.order_id and line.order_id.state in ['draft', 'sent']:
                 product_name = line.product_id.name or "Product"
-                line.order_id._increment_revision(f"Added product line: {product_name} (Qty: {line.product_uom_qty})")
+                line.order_id._increment_revision(f"Product Added: {product_name} (Qty: {line.product_uom_qty})")
         return lines
 
     def write(self, vals):
