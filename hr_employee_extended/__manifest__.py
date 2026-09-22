9242e234692def3b575cc20ea263467dea67eec0
dericking01 <dericking01@gmail.com>
2026-09-22 20:56:35 +0000

    Refactor code formatting and update model definitions in HR Employee Extended module

--- hr_employee_extended/__manifest__.py
+++ hr_employee_extended/__manifest__.py
@@ -7,7 +7,7 @@
         - Tax Identification Number (TIN)
         - National Social Security Fund (NSSF) Number
     """,
-    'author': 'Derrick Kings',
+    'author': 'Derrick King',
     'website': 'https://www.pcl.co.tz',
     'category': 'Human Resources',
     'depends': ['hr','hr_contract'],
