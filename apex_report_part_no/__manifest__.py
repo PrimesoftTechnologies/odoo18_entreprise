3c27d7ebdcd361bdd898cb4cc58b15bb37174c2f
MsamyAzizy <azizomary99@gmail.com>
2026-09-25 11:15:05 +0300

    engineering

---
+++ apex_report_part_no/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Apex Report Part No',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Removes internal reference from quotation and order reports',
+    'author': 'Apex',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_report_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
